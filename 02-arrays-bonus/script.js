7bb9760539c967ee33730e3c4234e1b98e3c35af
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-04 16:11:33 +0100

    Add 3th task for 02-arrays-bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -11,13 +11,15 @@ const teachers = [
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
 const reversedTeachers = null;
+;
+
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = null;
 
 // 3. Rimuovi 'Ed' dall'array teachers
-
+teachers.splice(1, 1)
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 const isFabioPresent = teachers.includes('Fabio');
