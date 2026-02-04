dce2bafd65e53cdbbc0eb2a79c90db41f6214daa
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-04 18:58:27 +0100

    Add 2th Task for 02-arrays-bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -23,7 +23,16 @@ for (let i = teachers.length; i > 0; i--) {
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+let longNames = null;
+
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5){
+    longNames = teachers[i]
+    console.log('Gli insegnanti con un nome >= a 5 cartteri', longNames);
+    
+}
+
+}
 
 
 // 3. Rimuovi 'Ed' dall'array teachers
@@ -31,12 +40,12 @@ teachers.splice(1, 1)
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 const isFabioPresent = teachers.includes('Fabio');
-console.log(isFabioPresent);
+console.log('Se Fabio è presenta tra gli insegnanti dai un stampa il valore true', isFabioPresent);
 
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = teachers.join();
-console.log(teachersString);
+console.log('Unisci tutti gli insegnanti nell array teachers in una stringa  separata da virgole', teachersString);
 
 
 
