cd3a4a191b839c90c14589592a1aa0ac11eabbee
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-04 15:21:04 +0100

    Add 3th task for 01-arrays

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -15,11 +15,14 @@ console.log(fourthTeacher);
 
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-teachers[4] = 'Patrick'
+teachers[4] = 'Patrick';
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+  console.log(lastTeacher);
+  
+
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
