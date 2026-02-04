44bc5bea0d13a45a147d7faab9e162cea009101a
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-04 15:54:31 +0100

    Add 5th task for o2-arrays-bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -25,4 +25,9 @@ console.log(isFabioPresent);
 
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.join();
+console.log(teachersString);
+
+
+
+
