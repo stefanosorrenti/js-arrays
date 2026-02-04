5e140b435d9b3af2396444d64b901b20b07e9503
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-04 16:38:40 +0100

    Add 1th task for 02-arrays-bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,8 +10,10 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
-;
+const reversedTeachers = teachers.toReversed();
+
+console.log('Lista isnegnanti al contrario.', reversedTeachers);
+
 
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
