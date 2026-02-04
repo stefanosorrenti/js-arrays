113262b65394732ca5826d2bbcdff8790f671049
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-04 15:23:37 +0100

    Add 5th task for 01-arrays

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -30,6 +30,8 @@ console.log(firstTeacher);
 
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push('Vanessa')
+console.log(teachers);
 
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
